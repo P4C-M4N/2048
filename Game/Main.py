4fdc0ab98b80804453daeaef6ab7b07762fd78f7
P4C-M4N <80701638+P4C-M4N@users.noreply.github.com>
2023-12-05 09:41:43 +0100

    calc moyenne

--- Game/Main.py
+++ Game/Main.py
@@ -1,9 +1,18 @@
 from Game import Game
 from GameIA import GameIA
 
+
+
 if __name__ == "__main__":
-    game = GameIA()
-    game.start()
+    scores = []
+    for i in range(100):
+        print("Partie ", i)
+        game = GameIA()
+        game.start()
+        scores.append(game.grille.score)
+        
+    print ("\n#########################################\nMoyenne des scores : ", sum(scores)/len(scores))
+    
 
 
 
