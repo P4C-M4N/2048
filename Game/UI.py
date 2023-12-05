451446de838e6d872cf6d9d2b40d1292e2cf8fb8
Fournet Enzo <enzofournet31@gmail.com>
2023-12-05 16:27:08 +0100

    Update UI.py

--- Game/UI.py
+++ Game/UI.py
@@ -6,6 +6,10 @@ from ia import Ia
 # Initialiser Pygame
 pygame.init()
 
+# Méthode pour centrer un bouton sur l'écran
+def centrer_bouton(x, y, largeur_ecran, hauteur_ecran, largeur_bouton, hauteur_bouton):
+    return (largeur_ecran * x - largeur_bouton / 2, hauteur_ecran * y - hauteur_bouton / 2)
+
 # Constantes pour l'interface
 TAILLE_CASE = 100
 MARGE = 10
@@ -14,9 +18,11 @@ TAILLE_ECRAN = (TAILLE_CASE * 4 + MARGE * 5, TAILLE_CASE * 4 + MARGE * 6 + TAILL
 COULEURS = {0: (204, 192, 179), 2: (238, 228, 218), 4: (237, 224, 200), 8: (242, 177, 121), 
             16: (245, 149, 99), 32: (246, 124, 95), 64: (246, 94, 59), 128: (237, 207, 114), 
             256: (237, 204, 97), 512: (237, 200, 80), 1024: (237, 197, 63), 2048: (237, 194, 46)}
-POSITION_BOUTON_MANUEL = (50, TAILLE_CASE * 4 + MARGE * 5)
-POSITION_BOUTON_IA = (300, TAILLE_CASE * 4 + MARGE * 5)
-POSITION_BOUTON_ARRET = (550, TAILLE_CASE * 4 + MARGE * 5)
+
+POSITION_BOUTON_MANUEL = centrer_bouton(0.5, 0.7, TAILLE_ECRAN[0], TAILLE_ECRAN[1], TAILLE_BOUTON[0], TAILLE_BOUTON[1])
+POSITION_BOUTON_IA = centrer_bouton(0.5, 0.8, TAILLE_ECRAN[0], TAILLE_ECRAN[1], TAILLE_BOUTON[0], TAILLE_BOUTON[1])
+POSITION_BOUTON_ARRET = centrer_bouton(0.5, 0.9, TAILLE_ECRAN[0], TAILLE_ECRAN[1], TAILLE_BOUTON[0], TAILLE_BOUTON[1])
+
 COULEUR_BOUTON = (100, 100, 100)
 COULEUR_TEXTE = (255, 255, 255)
 
@@ -30,10 +36,10 @@ pygame.display.set_caption('2048')
 grille = Grille()
 ia = Ia(grille)
 
+# Fonction dessiner_boutons
 def dessiner_boutons():
     ecran.fill((128, 128, 128))  # Changer la couleur de fond en gris pour une meilleure visibilité
     font = pygame.font.Font(None, 40)
-    
     pygame.draw.rect(ecran, COULEUR_BOUTON, (*POSITION_BOUTON_MANUEL, *TAILLE_BOUTON))
     pygame.draw.rect(ecran, COULEUR_BOUTON, (*POSITION_BOUTON_IA, *TAILLE_BOUTON))
     pygame.draw.rect(ecran, COULEUR_BOUTON, (*POSITION_BOUTON_ARRET, *TAILLE_BOUTON))
@@ -46,11 +52,10 @@ def dessiner_boutons():
     ecran.blit(texte_IA, (POSITION_BOUTON_IA[0] + 40, POSITION_BOUTON_IA[1] + 10))
     ecran.blit(texte_arret, (POSITION_BOUTON_ARRET[0] + 50, POSITION_BOUTON_ARRET[1] + 10))
 
-
-
-
 def dessiner_grille():
     ecran.fill((187, 173, 160))
+    
+    # Dessiner la grille
     for i in range(4):
         for j in range(4):
             valeur = grille.grille[i][j]
@@ -63,6 +68,11 @@ def dessiner_grille():
                 texte = font.render(str(valeur), True, (0, 0, 0))
                 ecran.blit(texte, (j * TAILLE_CASE + (j + 1) * MARGE + 30, 
                                    i * TAILLE_CASE + (i + 1) * MARGE + 30))
+    # Dessiner le bouton "Quitter"
+    font = pygame.font.Font(None, 40)
+    pygame.draw.rect(ecran, COULEUR_BOUTON, (*POSITION_BOUTON_ARRET, *TAILLE_BOUTON))
+    texte_arret = font.render('Quitter', True, COULEUR_TEXTE)
+    ecran.blit(texte_arret, (POSITION_BOUTON_ARRET[0] + 50, POSITION_BOUTON_ARRET[1] + 10))
                 
 def bouton_clique(pos, pos_bouton):
     x, y = pos
@@ -90,8 +100,9 @@ while True:
                     mode_IA = True
                     grille = Grille()  # Réinitialiser la grille
                 elif bouton_clique(event.pos, (*POSITION_BOUTON_ARRET, *TAILLE_BOUTON)):
-                    mode_IA = None
-                    dessiner_boutons()
+                    if mode_IA is None:
+                        pygame.quit()
+                        sys.exit()
         elif mode_IA:
             # Exécuter l'IA pour faire un mouvement
             move = ia.calculMeilleurCoup()
