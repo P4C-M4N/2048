a689d7bbe0934f5f5b2c90aaf416ddd6aa13aea2
P4C-M4N <80701638+P4C-M4N@users.noreply.github.com>
2023-12-06 19:39:11 +0100

    update

--- Game/ia.py
+++ Game/ia.py
@@ -4,6 +4,41 @@ import neat
 from Grille import Grille
 from neat.parallel import ParallelEvaluator
 from neat import StdOutReporter, StatisticsReporter
+from math import log2
+
+import math
+
+def rotate_clockwise(arr, iteration = 1):
+    if iteration <= 0:
+        return
+
+    l = len(arr)
+    for i in range(0, iteration):
+        for s in range(0, int(l / 2)):
+            for j in range(0, l - (2 * s) - 1):
+                temp = arr[s][s + j]
+                arr[s][s + j] = arr[l - s - j - 1][s]
+                arr[l - s - j - 1][s] = arr[l - s - 1][l - s - j - 1]
+                arr[l - s - 1][l - s - j - 1] = arr[s + j][l - s - 1]
+                arr[s + j][l - s - 1] = temp
+
+    return arr
+
+def calc_smoothness(grille):
+    board = grille.grille
+    smoothness = 0
+    # Only rotate twice to avoid double counting
+    # Ignore 0 tiles
+    for rotation in range(2):
+        for i in range(0, len(board)):
+            for j in range(0, len(board[i])):
+                if board[i][j] != 0 and j + 1 < len(board[i]) and board[i][j + 1] != 0:
+                    current_smoothness = math.fabs(log2(board[i][j]) - log2(board[i][j + 1]))
+                    smoothness = smoothness - current_smoothness
+        rotate_clockwise(board)
+
+    return smoothness
+
 
 def CoupPossible(grille):
         dirf = []
@@ -18,6 +53,8 @@ def CoupPossible(grille):
 
         return dirf
 
+score_w = 1.0
+smoothness_w = 1.0
 def eval_genome(genome, config):
     net = neat.nn.FeedForwardNetwork.create(genome, config)
     grille = Grille()
@@ -34,7 +71,9 @@ def eval_genome(genome, config):
     bestCase = grille.bestCase()
     print(f"Score: {grille.score}, BestCase: {bestCase}")
     grille.ajoutNombreAleatoire()
-    return grille.score  # Renvoie le score comme fitness
+    score = grille.score
+    smoothness = calc_smoothness(grille)
+    return (grille.score * score_w / (smoothness * smoothness_w)) * log2(bestCase) * -1  # Renvoie le score comme fitness
 
 def print_generation_info(population):
     print(f"Generation: {population.generation}")
