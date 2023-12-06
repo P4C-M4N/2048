699929481e55fac37f44ebee21df948222784cf9
Tructruc <emilien.fieu@etu.univ-tlse2.fr>
2023-12-06 13:16:14 +0100

    Nul

--- Game/ia.py
+++ Game/ia.py
@@ -3,46 +3,6 @@ from Grille import Grille
 import copy
 import neat
 
-def caluculate_monoticity(grille):
-    """
-    This heuristic tries to ensure that the values of the tiles are all either
-    increasing or decreasing along both the left/right and up/down directions.
-    This heuristic alone captures the intuition that many others have mentioned,
-    that higher valued tiles should be clustered in a corner. It will typically
-    prevent smaller valued tiles from getting orphaned and will keep the board
-    very organized, with smaller tiles cascading in and filling up into the larger tiles.
-
-    Here's a screenshot of a perfectly monotonic grid. I obtained this by running the
-    algorithm with the eval function set to disregard the other heuristics and only
-    consider monotonicity.
-    :param grille:
-    :return:
-    """
-
-
-    # left/right
-    left_right = 0
-    for i in range(4):
-        for j in range(3):
-            if grille[i][j] > grille[i][j + 1]:
-                left_right += 1
-            elif grille[i][j] < grille[i][j + 1]:
-                left_right -= 1
-
-    # up/down
-    up_down = 0
-    for j in range(4):
-        for i in range(3):
-            if grille[i][j] > grille[i + 1][j]:
-                up_down += 1
-            elif grille[i][j] < grille[i + 1][j]:
-                up_down -= 1
-
-    return max(left_right, up_down)
-
-
-
-
 
 def eval_genomes(genomes, config):
     scores = []
@@ -61,7 +21,6 @@ def eval_genomes(genomes, config):
 
             if grille.TryDeplacement(move):
                 genome.fitness += 20
-                genome.fitness += caluculate_monoticity(grille.grille)*2
 
 
                 grille.ajoutNombreAleatoire()
@@ -92,14 +51,6 @@ if __name__ == "__main__":
 
     p = neat.Population(config)
 
-    p.run(eval_genomes, 500)
+    p.run(eval_genomes, 100000)
 
     winner = p.best_genome
-
-        
-
-
-
-
-
-
