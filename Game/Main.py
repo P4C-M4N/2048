39c90f96a7d8817f21a5eb1848f8e40911bc76fe
Fournet Enzo <enzofournet31@gmail.com>
2023-12-07 18:48:38 +0100

    docker et simplicité du main t'inquiete

--- Game/Main.py
+++ Game/Main.py
@@ -1,14 +1,20 @@
 from concurrent.futures import ProcessPoolExecutor
 from GameIA import GameIA
 from tqdm import tqdm
+import argparse
 
 def jouer_partie(_):
     game = GameIA()
     game.start()
     return game.grille.score, game.grille
 
-if __name__ == "__main__":
-    nb_parties = 300
+def main(nb_parties_arg):
+    if isinstance(nb_parties_arg, int) and nb_parties_arg > 0:
+        nb_parties = nb_parties_arg
+        print("Nombre de parties : ", nb_parties)
+    else:
+        nb_parties = 50
+        print("Nombre de parties invalide, utilisation de la valeur par défaut (50)")
 
     with ProcessPoolExecutor() as executor:
         resultats = list(tqdm(executor.map(jouer_partie, range(nb_parties)), total=nb_parties, desc="Simulation Progress"))
@@ -29,3 +35,9 @@ if __name__ == "__main__":
                 break
 
     print("Nombre de parties gagnées : ", nb_parties_gagnees)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Jouer un nombre spécifié de parties.")
+    parser.add_argument("nb_parties", type=int, nargs='?', default=50, help="Nombre de parties à jouer")
+    args = parser.parse_args()
+    main(args.nb_parties)
--- dockerfile
+++ dockerfile
@@ -1,6 +1,18 @@
 #Alpine based image for python 3.11.5
 FROM python:3.11.5-alpine
+
+#Copy the requirements.txt file and all the files
 COPY ./Game /Game
+COPY ./requirements.txt /requirements.txt
+
+#Working directory for installation
+WORKDIR /
+
+#Install the requirements
+RUN pip install -r requirements.txt
+
+#Set the working directory
 WORKDIR /Game
+
 #Tail for infinite loop
 CMD tail -f /dev/null
