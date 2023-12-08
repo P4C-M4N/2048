e9a18771eb11024bd182c1eaf99568bb800f8313
Fournet Enzo <enzofournet31@gmail.com>
2023-12-08 11:16:16 +0100

    Update ia.py

--- Game/ia.py
+++ Game/ia.py
@@ -1,3 +1,4 @@
+import math
 from Grille import Grille
 import copy
 
@@ -20,16 +21,16 @@ class Ia:
         for i in range(tailleGrille):
             for j in range(tailleGrille):
                 valeurCase = grille[i][j]
-                # Plus la valeur est élevée et proche du coin, plus le score est élevé
                 scoreProximite = (tailleGrille - i) * (tailleGrille - j)
                 scoreEvaluation += valeurCase * scoreProximite
 
-                # Encourage les valeurs adjacentes similaires
                 if j < tailleGrille - 1:
                     scoreEvaluation -= abs(valeurCase - grille[i][j + 1])
                 if i < tailleGrille - 1:
                     scoreEvaluation -= abs(valeurCase - grille[i + 1][j])
-        return scoreEvaluation
+        
+        monotonicite = self.calc_monotonicite(grille)
+        return scoreEvaluation + monotonicite
 
     def calculMeilleurCoupProfondeur(self, grille, profondeur):
         if profondeur == 0:
@@ -46,7 +47,9 @@ class Ia:
                 scoreEvaluation = self.evaluationGrille(grilleCopie.grille)
                 pointsCoin = self.pointsPourCoin(grilleCopie.grille)
                 pointsCaseVide = self.pointsPourCasesVides(grilleCopie.grille)
-                scoreTotal = grilleCopie.score + scoreFutur + scoreEvaluation + pointsCoin + pointsCaseVide
+                pointsSmoothness = self.calc_smoothness(grilleCopie)
+                pointMonotonicite = self.calc_monotonicite(grilleCopie.grille)
+                scoreTotal = grilleCopie.score + scoreFutur + scoreEvaluation + pointsCoin + pointsCaseVide + pointsSmoothness + pointMonotonicite
                 meilleursScores.append(scoreTotal)
 
         return max(meilleursScores) if meilleursScores else -1
@@ -63,6 +66,69 @@ class Ia:
 
         return self.meilleurSituation(grilles)
 
+    # 2D array, rotates by 90 degrees
+    @staticmethod
+    def rotate_clockwise(arr, iteration=1):
+        n = len(arr)
+        for _ in range(iteration):
+            for x in range(n // 2):
+                for y in range(x, n - x - 1):
+                    temp = arr[x][y]
+                    arr[x][y] = arr[n - y - 1][x]
+                    arr[n - y - 1][x] = arr[n - x - 1][n - y - 1]
+                    arr[n - x - 1][n - y - 1] = arr[y][n - x - 1]
+                    arr[y][n - x - 1] = temp
+        return arr
+
+    def calc_smoothness(self, grille):
+        board = copy.deepcopy(grille.grille)
+        smoothness = 0
+        # Only rotate twice to avoid double counting
+        # Ignore 0 tiles
+        for rotation in range(2):
+            for i in range(0, len(board)):
+                for j in range(0, len(board[i])):
+                    if board[i][j] != 0 and j + 1 < len(board[i]) and board[i][j + 1] != 0:
+                        current_smoothness = math.fabs(math.log2(board[i][j]) - math.log2(board[i][j + 1]))
+                        smoothness = smoothness - current_smoothness
+            self.rotate_clockwise(board)
+
+        return smoothness
+    
+    def calc_monotonicite(self, grille):
+       scores = [0, 0, 0, 0]  # Haut/Bas, Bas/Haut, Gauche/Droite, Droite/Gauche
+       for i in range(4):
+           current = 0
+           next = current + 1
+           while next < 4:
+               while next < 4 and grille[i][next] == 0:
+                   next += 1
+               if next >= 4: next -= 1
+               currentValue = math.log(grille[i][current], 2) if grille[i][current] != 0 else 0
+               nextValue = math.log(grille[i][next], 2) if grille[i][next] != 0 else 0
+               if currentValue > nextValue:
+                   scores[0] += nextValue - currentValue
+               elif nextValue > currentValue:
+                   scores[1] += currentValue - nextValue
+               current = next
+               next += 1
+       for j in range(4):
+           current = 0
+           next = current + 1
+           while next < 4:
+               while next < 4 and grille[next][j] == 0:
+                   next += 1
+               if next >= 4: next -= 1
+               currentValue = math.log(grille[current][j], 2) if grille[current][j] != 0 else 0
+               nextValue = math.log(grille[next][j], 2) if grille[next][j] != 0 else 0
+               if currentValue > nextValue:
+                   scores[2] += nextValue - currentValue
+               elif nextValue > currentValue:
+                   scores[3] += currentValue - nextValue
+               current = next
+               next += 1
+       return max(scores)
+
     def meilleurSituation(self, grilles):
         scores = {}
         for direction, grille in grilles.items():
@@ -71,7 +137,9 @@ class Ia:
             pointsCoin = self.pointsPourCoin(grille.grille)
             scoreProfondeur = self.calculMeilleurCoupProfondeur(grille, 3)
             pointsCaseVide = self.pointsPourCasesVides(grille.grille)
-            scoreTotal = scoreBase + scoreEvaluation + pointsCoin + scoreProfondeur + pointsCaseVide
+            pointsSmoothness = self.calc_smoothness(grille)
+            pointsMonotonicite = self.calc_monotonicite(grille.grille)
+            scoreTotal = scoreBase + scoreEvaluation + pointsCoin + scoreProfondeur + pointsCaseVide + pointsSmoothness + pointsMonotonicite
             scores[direction] = scoreTotal
 
         return max(scores, key=scores.get) if scores else None
